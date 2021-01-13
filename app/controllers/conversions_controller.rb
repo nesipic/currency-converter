024cbce70cc6465fce32f274a687d1e3432117d7
NedijkoSipic <nesipic@hotmail.com>
2021-01-13 01:10:37 +0100

    backend done, styling need more work

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,10 +1,33 @@
 class ConversionsController < ApplicationController
+  require 'date'
+  require 'money/bank/currencylayer_bank'
   def index
+    @conversions = Conversion.where(user: current_user) 
   end
   
   def new
+    @conversion = Conversion.new
   end
   
   def create
+    @conversion = Conversion.new(strong_params)
+    @conversion.user = current_user
+    @conversion.date = Date.today
+    @conversion.result = convertion(@conversion.amount, @conversion.from_currency, @conversion.to_currency)
+    @conversion.save ? (redirect_to new_conversion_path) : (render :new)
+  end
+
+  private
+  def strong_params
+    params.require(:conversion).permit(:from_currency, :to_currency, :amount)
+  end
+
+  def convertion(amount, from, to)
+    return nil if amount.class == String || amount == nil
+    mclb = Money::Bank::CurrencylayerBank.new
+    mclb.access_key = '8c28ef8d5b153d2f93974101c17e6b12'
+    mclb.update_rates
+    rate = mclb.get_rate(from, to)
+    (amount * rate.round(2)).round(2)
   end
 end
--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,3 +1,21 @@
 class Conversion < ApplicationRecord
   belongs_to :user
+  validates :from_currency, presence: true
+  validates :to_currency, presence: true
+  validates :amount, numericality: true, presence: true
+  validate :custom_validations
+  
+  private
+  def custom_validations
+    if from_currency == to_currency
+      errors.add(:to_currency,"can't be the same as base currency")
+    end
+    if !(["USD", "EUR", "CHF"].include? to_currency)
+      errors.add(:to_currency,'must be one of the following "USD","EUR" or "CHF"' )
+    end
+    if !(["USD", "EUR", "CHF"].include? from_currency)
+      errors.add(:to_currency,'must be one of the following "USD","EUR" or "CHF"' )
+    end
+  end
+  
 end
